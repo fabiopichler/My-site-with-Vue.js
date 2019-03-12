520e3db500e0e5e9a23b7e662f06b83627a5b369
Fábio Pichler <fabiopichler1992@gmail.com>
2019-03-12 09:29:16 -0300

    Layout improvements

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -1,13 +1,13 @@
 <template>
     <base-section name="about" title="Sobre o Projeto" titleicon="fas fa-info-circle">
         <div class="media">
-            <img src="../../assets/logo.png" class="align-self-center mr-3" alt="Vue.js logo" width="40" height="40">
+            <img src="../../assets/logo.png" class="align-self-center mr-3" alt="Vue.js logo" width="50" height="50">
 
             <div class="media-body">
                 <p class="mb-0">
-                    Desenvolvido com <a href="https://vuejs.org/" target="_blank"><strong>Vue.js</strong></a>
+                    Desenvolvido com <a href="https://vuejs.org/" target="_blank">Vue.js</a>
                     <br>
-                    » <a href="https://github.com/fabiopichler/My-site-with-Vue.js" target="_blank">Repositório no GitHub</a>
+                    » <a href="https://github.com/fabiopichler/My-site-with-Vue.js" target="_blank"><strong>Repositório no GitHub</strong></a>
                     <br>
                     » <a href="https://github.com/fabiopichler/My-basic-REST-API" target="_blank">API REST no GitHub</a> 
                 </p>
